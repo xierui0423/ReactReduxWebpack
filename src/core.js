971c82bbbad1859d97816f496295a101a32fb4f4
Ray Xie <xierui0423@gmail.com>
2016-09-09 17:11:11 +0800

    core pure functions done with unit tests

--- src/core.js
+++ src/core.js
@@ -2,8 +2,42 @@
  * Created by ray.xie on 9/8/2016.
  */
 
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
-export default function setEntries(state, entries) {
+function getWinner(voteState) {
+  const [firstEntry, lastEntry] = voteState.get('pair', List.of('', '')).toArray();
+
+  // Return no winner if there is no tally
+  if (!firstEntry || !lastEntry) return [];
+
+  const [firstEntryTally, lastEntryTally] =
+    [voteState.getIn(['tally', firstEntry], 0), voteState.getIn(['tally', lastEntry], 0)];
+
+  if (firstEntryTally > lastEntryTally) return [firstEntry];
+  else if (firstEntryTally < lastEntryTally) return [lastEntry];
+
+  return [firstEntry, lastEntry];
+}
+
+export function setEntries(state, entries) {
   return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  const winner = getWinner(state.get('vote', Map()));
+
+  const entries = state.get('entries').concat(winner);
+
+  // We got the final winner, hooray!
+  if (entries.size === 1) {
+    return state.remove('vote').remove('entries').set('winner', entries.first());
+  }
+
+  return state.merge({ vote: Map({ pair: entries.take(2) }), entries: entries.skip(2) });
+}
+
+export function vote(state, entry) {
+  if (!state.getIn(['vote', 'pair'], List()).contains(entry)) return state;
+
+  return state.updateIn(['vote', 'tally', entry], 0, val => val + 1);
 }
--- test/core.js
+++ test/core.js
@@ -4,7 +4,7 @@
 
 import { expect } from 'chai';
 import { List, Map } from 'immutable';
-import setEntries from '../src/core';
+import { setEntries, next, vote } from '../src/core';
 
 
 describe('application logic', () => {
@@ -14,17 +14,147 @@ describe('application logic', () => {
       const entries = List.of('topic1', 'topic2');
       const nextState = setEntries(state, entries);
 
-      expect(state).to.equal(Map());
       expect(nextState).to.equal(Map({ entries: List.of('topic1', 'topic2') }));
     });
 
-    it('convers the entries to immutable', () => {
+    it('converts the entries to immutable', () => {
       const state = Map();
       const entries = ['topic1', 'topic2'];
       const nextState = setEntries(state, entries);
 
-      expect(state).to.equal(Map());
       expect(nextState).to.equal(Map({ entries: List.of('topic1', 'topic2') }));
+    });
+  });
+
+  describe('next', () => {
+    it('takes two entries into the vote map', () => {
+      const state = Map({ entries: List.of('topic1', 'topic2', 'topic3') });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2') }),
+        }));
+    });
+
+    it('puts winner entry back into the entry list, dump the loser entry', () => {
+      // To test the situation that one entry has not been voted
+      let state = Map({
+        vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic1: 3 }) }),
+        entries: List.of('topic3', 'topic4'),
+      });
+      state = next(state);
+
+      expect(state).to.equal(Map(
+        {
+          entries: List.of('topic1'),
+          vote: Map({ pair: List.of('topic3', 'topic4') }),
+        }));
+
+      // To test the situation that both entries have been voted
+      state = Map({
+        vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic1: 4, topic2: 5 }) }),
+        entries: List.of('topic3', 'topic4'),
+      });
+
+      state = next(state);
+
+      expect(state).to.equal(Map(
+        {
+          entries: List.of('topic2'),
+          vote: Map({ pair: List.of('topic3', 'topic4') }),
+        }));
+    });
+
+    it('puts winner entries (on tie) back into the entry list', () => {
+      // To test the situation that both entries have not been voted
+      let state = Map({
+        vote: Map({ pair: List.of('topic1', 'topic2') }),
+        entries: List.of('topic3', 'topic4'),
+      });
+
+      state = next(state);
+
+      expect(state).to.equal(Map(
+        {
+          entries: List.of('topic1', 'topic2'),
+          vote: Map({ pair: List.of('topic3', 'topic4') }),
+        }));
+
+      // To test the situation that both entries have been voted
+      state = Map({
+        vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic1: 5, topic2: 5 }) }),
+        entries: List.of('topic3', 'topic4'),
+      });
+
+      state = next(state);
+
+      expect(state).to.equal(Map(
+        {
+          entries: List.of('topic1', 'topic2'),
+          vote: Map({ pair: List.of('topic3', 'topic4') }),
+        }));
+    });
+
+    it('displays the final winner when there is only one entry left', () => {
+      let state = Map({
+        vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic1: 2, topic2: 1 }) }),
+        entries: List(),
+      });
+
+      state = next(state);
+
+      expect(state).to.equal(Map(
+        {
+          winner: 'topic1',
+        }));
+    });
+  });
+
+  describe('vote', () => {
+    it('creates a new tally for a new entry', () => {
+      const state = Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2') }),
+        });
+
+      const nextState = vote(state, 'topic2');
+
+      expect(nextState).to.equal(Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic2: 1 }) }),
+        }));
+    });
+
+    it('increases vote for the entry with tally', () => {
+      const state = Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic2: 1 }) }),
+        });
+
+      const nextState = vote(state, 'topic2');
+
+      expect(nextState).to.equal(Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic2: 2 }) }),
+        }));
+    });
+
+    it('does nothing when invalid entry name is given', () => {
+      const state = Map(
+        {
+          entries: List.of('topic3'),
+          vote: Map({ pair: List.of('topic1', 'topic2'), tally: Map({ topic2: 1 }) }),
+        });
+
+      const nextState = vote(state, 'topic3');
+
+      expect(nextState).to.equal(state);
     });
   });
 });
